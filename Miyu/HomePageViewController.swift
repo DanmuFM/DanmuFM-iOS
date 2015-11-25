af03a6287c55d46d4a385bf8d264b95f75c0849a
Cee <cee@chu2byo.com>
2015-11-25 14:30:26 +0800

    Initialize the music info page

---
+++ Miyu/HomePageViewController.swift
@@ -0,0 +1,60 @@
+//
+//  HomePageViewController.swift
+//  Miyu
+//
+//  Created by Cee on 25/11/2015.
+//  Copyright © 2015 Cee. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIPageViewController {
+    
+    var pages = [UIViewController]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.delegate = self
+        self.dataSource = self
+        
+        let page1: UIViewController! = storyboard?.instantiateViewControllerWithIdentifier("page1")
+        let page2: UIViewController! = storyboard?.instantiateViewControllerWithIdentifier("page1")
+        
+        pages.append(page1)
+        pages.append(page2)
+        
+        setViewControllers([page1], direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+extension HomePageViewController: UIPageViewControllerDataSource {
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        let currentIndex = pages.indexOf(viewController)!
+        let previousIndex = abs((currentIndex - 1) % pages.count)
+        return pages[previousIndex]
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        let currentIndex = pages.indexOf(viewController)!
+        let nextIndex = abs((currentIndex + 1) % pages.count)
+        return pages[nextIndex]
+    }
+}
+
+extension HomePageViewController: UIPageViewControllerDelegate {
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return pages.count
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+}
